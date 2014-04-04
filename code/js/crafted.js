3ecd265c6fbf78541fecaa21f045156c3e5bbdba
hepapatitis <hepapatitis@yahoo.com>
2014-04-04 15:11:09 +1100

    New Contact Form, New BG for top, Fix on Nav

--- code/js/crafted.js
+++ code/js/crafted.js
@@ -1,9 +1,13 @@
 // Nice Scroll for #
 $("a").click(function() {
 	var elementClicked = $(this).attr("href");
-	var destination = $(elementClicked).offset().top;
+	if(elementClicked.indexOf("#") > -1)
+	{
+		var destination = $(elementClicked).offset().top;
+		
+		$("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 1000 );
+	}
 	
-	$("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 1000 );
 	return false;
 });
 
@@ -13,30 +17,45 @@ function resize_section()
 }
 
 function toggle_nav() {
-	var nav = $(".fixed-nav");
+	var nav_container = $(".fixed-nav");
 	var nav_toggler = $("#nav-toggler");
 	
-	if(nav.hasClass("opened"))
+	if(nav_container.hasClass("opened"))
 	{
-		nav.removeClass("opened");
-		nav.addClass("closed");
-		nav_toggler.html('<i class="fa fa-angle-double-up"></i>');
+		nav_container.removeClass("opened");
+		nav_container.addClass("closed");
 	}
 	else
 	{
-		nav.removeClass("closed");
-		nav.addClass("opened");
+		nav_container.removeClass("closed");
+		nav_container.addClass("opened");
+	}
+	refresh_nav();
+}
+
+function refresh_nav()
+{
+	var nav_container = $(".fixed-nav");
+	var nav_toggler = $("#nav-toggler");
+	
+	if(nav_container.hasClass("opened"))
+	{
+		nav_container.css("bottom",-8);
 		nav_toggler.html('<i class="fa fa-angle-double-down"></i>');
+	}
+	else
+	{
+		nav_container.css("bottom",0-nav_container.height());
+		nav_toggler.html('<i class="fa fa-angle-double-up"></i>');
 	}
 }
 
 $(function() {
 	resize_section();
-	toggle_nav();
+	refresh_nav();
 	
-		
-
 	$(window).resize(function() {
 		resize_section();
+		refresh_nav();
 	});
 });
